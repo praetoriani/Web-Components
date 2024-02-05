74dc3b67980cc897dbc3693392f71ba7fd1e5c77
praetoriani <marc.sczepanski@gmail.com>
2024-02-05 14:22:25 +0100

    changelog
    website updated

--- docs/site.js
+++ docs/site.js
@@ -5,24 +5,8 @@ https://github.com/praetoriani/Web-Components
 
 // Set a new URL
 function LoadContentPage(PageName) {
-    // Get the URL and remove the filename
-    var FullURL = window.location.href;
-    var lastIndex = FullURL.lastIndexOf("/");
-    FullURL = FullURL.substring(0, lastIndex + 1);
-
-    // Check if we are in a sub-folder
-    if (FullURL.endsWith("web-components/")) {
-        // If yes, we're shorten the URL to get the root-folder
-        FullURL = FullURL.substring(0, FullURL.length - "web-components/".length);
-    }
-
-    // Check if we are in a sub-folder
-    if (FullURL.endsWith("demo/")) {
-        // If yes, we're shorten the URL to get the root-folder
-        FullURL = FullURL.substring(0, FullURL.length - "web-components/demo/".length);
-    }
-
-    window.location.assign(FullURL+PageName);
+    let RootURL = "https://praetoriani.github.io/web-components/";
+    window.location.assign(RootURL+PageName);
 }
 
 
